53f32c354a51b1c1706ac644a560cfb85959e870
hemanta001 <rockingguyheman.hg@gmail.com>
2022-06-14 23:01:36 +0545

    second commit

---
+++ src/main/java/com/support/night/user/entity/User.java
@@ -0,0 +1,20 @@
+package com.support.night.user.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+}
