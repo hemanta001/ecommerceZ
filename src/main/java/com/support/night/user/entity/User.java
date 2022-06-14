c2fd10358c5bad388f4af6a259aac074d75d8677
hemanta001 <rockingguyheman.hg@gmail.com>
2022-06-14 23:08:12 +0545

    some changes

--- src/main/java/com/support/night/user/entity/User.java
+++ src/main/java/com/support/night/user/entity/User.java
@@ -1,10 +1,7 @@
 package com.support.night.user.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
@@ -17,4 +14,7 @@ public class User {
     private String lastName;
 
     private String username;
+
+    private String nickName;
+
 }
