a0e09d671bdd3f954111ec463645f19f8128547e
hemanta001 <rockingguyheman.hg@gmail.com>
2022-06-14 23:09:57 +0545

    some changes

--- src/main/java/com/support/night/user/entity/User.java
+++ src/main/java/com/support/night/user/entity/User.java
@@ -4,6 +4,7 @@ package com.support.night.user.entity;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
